9ef7ebb2feca823b085732ec78feadbe2979e011
Paszq <pasza1989@gmail.com>
2024-07-03 13:34:05 +0200

    Fixed image link for monsters to local - typo

--- script.js
+++ script.js
@@ -1029,7 +1029,7 @@ function calculateMonster(){
 
     let stats = {
         name: monsterName,
-        image: "images/"+monsterStats.Image.split("/"[7]),
+        image: "images/"+monsterStats.Image.split("/")[7],
         health: monsterStats.Health,
         exp: monsterStats.Exp,
         speed: monsterStats.Speed,
